3420f330d79c16ce685f400b1bee230e96351ea9
Esin <nbusra.esin@gmail.com>
2023-04-07 16:57:52 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+const employee = {
+    name : "esin"
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const updated = { ...employee} ;
+    updated[key] = value;
+    return updated
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee
+}
+
+function  deleteFromEmployeeByKey(employee, key){
+    let updated = { ...employee};
+    delete updated[key];
+    return updated 
+}
+
+function  destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee  
+}
